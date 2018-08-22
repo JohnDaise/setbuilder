f0113222a1b30b6ee544001db4d2cc05df87d4c3
JohnDaise <jdaise@gmail.com>
2018-08-22 15:47:28 -0400

    updated pseudocode and buttons

--- setbuilder-frontend/src/script.js
+++ setbuilder-frontend/src/script.js
@@ -6,45 +6,56 @@ document.addEventListener("DOMContentLoaded", function(){
   // renderSetlist();
 })
 
-function fetchSetlist(){
-  fetch(`http://localhost:3000/setlists`)
-  .then(r => r.json())
-    .then(data => {
-      data.forEach(setlist => {renderSetlistTitle(setlist)
-      });
-    });
-}
+
 
 function rightSidePanel(){
   let allSongsBtn = document.createElement('button')
+  let createSetlistBtn = document.createElement('button')
   let createSongButton = document.createElement('button')
   let rightSideDiv = document.getElementById("right-panel")
 
   allSongsBtn.innerText = "Repertoire"
   createSongButton.innerText = "Add New Song"
+  createSetlistBtn.innerText = "Create New Setlist"
 
   rightSideDiv.appendChild(allSongsBtn);
   rightSideDiv.appendChild(createSongButton);
+  rightSideDiv.appendChild(createSetlistBtn);
 
   allSongsBtn.addEventListener('click', renderAllSongs)
   createSongButton.addEventListener('click', createSongForm)
+  createSetlistBtn.addEventListener('click', createSetlist)
 
 };
 
+function fetchSetlist(){
+  fetch(`http://localhost:3000/setlists`)
+  .then(r => r.json())
+    .then(data => {
+      data.forEach(setlist => {renderSetlistTitle(setlist)
+        //function renderSetlistSongs to sidebar
+      });
+    });
+}
 
 function renderSetlistTitle(setlist){
   let sidebarDiv = document.getElementById("setlist-div")
   let h1 = document.createElement('h1')
   h1.innerText = setlist.title
   sidebarDiv.appendChild(h1)
+  //use this function to render setlist song names to sidebar as well
 };
+
+// function renderSetlistSongs(){
+//   //
+// }
 
 
 function fetchAllSongs(){
   fetch(`http://localhost:3000/songs`)
     .then(r => r.json())
       .then(data => {
-        data.forEach(song => {renderSong(song)
+        data.forEach(song => {renderSong(song) //should render to showPanel
         });
       });
 };
@@ -82,17 +93,23 @@ function renderNotes(id){
           let notes = document.createElement('p')
           let showPanel = document.getElementById("show-panel")
           let editBtn = document.createElement('button')
+          let deleteBtn = document.createElement('button')
+
           editBtn.innerText = "Edit Notes"
+          deleteBtn.innerText = "Delete from Set"
           editBtn.id = data.id
+          deleteBtn.id = data.id
 
           showPanel.innerHTML = ""
           showPanel.appendChild(header)
           showPanel.appendChild(notes)
           showPanel.appendChild(editBtn)
+          showPanel.appendChild(deleteBtn)
 
           header.innerText = data.name
           notes.innerText = data.notes
           editBtn.addEventListener('click', editNotes)
+          deleteBtn.addEventListener('click', deletefromSetlist)
           notes.contentEditable = "false"
           //for showPanel have key words come up as different colors for each named section of a song and display in big letters
       });
@@ -115,6 +132,11 @@ function editNotes(e){
     notes.contentEditable = !notes.contentEditable
     editBtn.innerText = "Edit Notes"
     };
+};
+
+
+function deletefromSetlist(){
+    console.log("Deleted")
 };
 
 
@@ -142,11 +164,30 @@ function updateSong(e){
    console.log('All Songs')
    //view all songs in the showPanel with just the title and a button to add song to setlist on the right
    //fetch all songs
-
+   fetch(`http://localhost:3000/songs`)
+     .then(r => r.json())
+       .then(data => {
+         data.forEach(song => {renderSongs(song)
+         });
+       });
  }
+
+ function renderSongs(){
+   console.log("All Songs")
+   //function fetchAllSongs()
+ }
+
+
 
 function createSongForm(){
  console.log('New Song')
  //will open up a new song form in the showPanel
  //form will have placeholder text for the name and will have contentEditable <p> with basic song form filled in
+ //will open up dropdown to add to or create setlist
 }
+
+function createSetlist(){
+  console.log('Create Setlist')
+  //will create a new setlist
+  // use oojs here
+};
